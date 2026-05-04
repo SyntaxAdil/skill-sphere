a4b24f24657e4fef6b1e9bb9fbc5990b02808b99
SyntaxAdil <abdurrahmanadil005@gmail.com>
2026-05-04 11:56:33 +0600

    Refactor API URL handling to use a consistent baseUrl variable

--- src/lib/courses.js
+++ src/lib/courses.js
@@ -1,11 +1,13 @@
+const baseUrl = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
+
 export const getCourses = async () => {
-  const course = await fetch(`${process.env.APP_URL}/data.json`);
+  const course = await fetch(`${baseUrl}/data.json`);
   const res = await course.json();
 
   return res;
 };
 export const getCourseById = async (id) => {
-  const course = await fetch(`${process.env.APP_URL}/data.json`);
+  const course = await fetch(`${baseUrl}/data.json`);
   const res = await course.json();
 
   const filter_course= res.filter((i) => i.id === Number(id));
